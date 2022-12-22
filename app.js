2b28eda3e22ebb664ccc4312dd9ac48551f637a9
Jimmyseesang <jimmyseesang@gmail.com>
2022-12-22 15:07:13 +0700

    update

--- app.js
+++ app.js
@@ -1,18 +1,18 @@
 const http = require('http')
-const sever = http.createServer((req,res)=>{
-    if(req.url == '/hello'){
-        res.writeHead(200,{'Content-type': 'text/html'})
-        res.write('Hello World!')
+const server = http.createServer((req,res) => {
+    if(req.url == '/'){
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        res.write('<h1>This is Home page!</h1>')
         res.end()
     }
     else if(req.url == '/student'){
-        res.writeHead(200,{'Content-type': 'text/html'})
-        res.write('<h1>This is student Page!</h1>')
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        res.write('<h1>This is Student Page!</h1>')
         res.end()
     }
     else if(req.url == '/admin'){
-        res.writeHead(200,{'Content-type': 'text/html'})
-        res.write('<h1>This is admin Page!</h1>')
+        res.writeHead(200, { 'Content-type': 'text/html' })
+        res.write('<h1>This is Amin Page!</h1>')
         res.end()
     }
     else{
@@ -20,6 +20,6 @@ const sever = http.createServer((req,res)=>{
     }
 })
 
-sever.listen(5000,()=> {
-    console.log(`Sever start at http://localhost:5000/`)
+server.listen(5000,()=>{
+    console.log(`Server start at http://localhost:5000`)
 })
